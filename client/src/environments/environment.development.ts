13266e066dd4cd880c5ac15fe0acea5c93822068
asenataluczak <asenata.luczak@gmail.com>
2024-05-27 21:17:27 +0200

    no one saw it

--- client/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  YT_API_KEY: 'AIzaSyCyn2zlWl2NCdRiS42T9a-3hg4Vv_wtHXE',
-};
--- client/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  YT_API_KEY: '',
-};
